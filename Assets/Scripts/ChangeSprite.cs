80323e351913d46eca723e42c775ca44ad877241
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-02 11:34:32 -0800

    002

---
+++ Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Changesprite()
+    {
+
+    }
+}
