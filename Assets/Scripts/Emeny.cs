76112f2c9ebc42179fbca1cb19ca19945cadc87e
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-02 09:36:09 -0800

    001

---
+++ Assets/Scripts/Emeny.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emeny : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    void Update()
+    {
+
+    }
+}
