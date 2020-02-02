035675ca8d01859cbbe00b4d3010d22eea561e4a
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-02 11:39:04 -0800

    changescene

--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -8,10 +8,12 @@ public class ChangeSprite : MonoBehaviour
     private int num = 0;
     private void Start()
     {
-        
+        GetComponent<SpriteRenderer>().sprite = sprites[num];
     }
     public void Changesprite()
     {
-        
+        if (num < sprites.Length) num += 1;
+        else num = 0;
+        GetComponent<SpriteRenderer>().sprite = sprites[num];
     }
 }
