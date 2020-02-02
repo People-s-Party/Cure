aecccb4eb8af12421cb578baa6c3295ce89a89b7
wang3ng <56563071+wang3ng@users.noreply.github.com>
2020-02-02 11:06:38 -0800

    Enemy

--- Assets/Scripts/Emeny.cs
+++ Assets/Scripts/Emeny.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class Emeny : MonoBehaviour
 {
     public float speed = 1.0f;
-    public GameObject player;
+    public Vector3 StartPostion;
     void Start()
     {
-        player = GameObject.Find("Player");
+        transform.position = Vector3.MoveTowards(transform.position, StartPostion, 10000);
     }
     void Update()
     {
-
+        Vector3 playerposition = transform.position;
+        float MoveX = 0;
+        float MoveY = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveY += Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MoveY -= Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveX -= Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveX += Time.deltaTime * speed;
+        }
+        if (MoveX != 0) transform.Translate(MoveX, 0, 0);
+        else transform.Translate(0, MoveY, 0);
     }
 }
