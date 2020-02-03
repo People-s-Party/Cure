65461d72c5aa8697d66c0cd73d0d249631200a85
Tonywww2 <1204484415@qq.com>
2020-02-02 16:50:28 -0800

    qwer

--- Assets/Scripts/EventDoorOpen.cs
+++ Assets/Scripts/EventDoorOpen.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class EventDoorOpen : MonoBehaviour
 {
-
+    public string doorID;
     void Update()
     {
-        if (GameObject.Find("EventSystem").GetComponent<PassDoorOpen>().active) gameObject.GetComponent<ItemInform>().needName = "";
+        if (GameObject.Find(doorID).GetComponent<PassDoorOpen>().active) gameObject.GetComponent<ItemInform>().needName = "";
         else gameObject.GetComponent<ItemInform>().needName = "Qifei";
     }
 }
